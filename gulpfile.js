dfd357d5e2e50021e84af8a5021d99fa1525d698
lavera <truongvanluong0503@gmail.com>
2021-06-03 06:38:42 +0700

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var runSequence = require('gulp4-run-sequence');
+//task biên dịch sass
+gulp.task('sass', function() {
+        return gulp.src('./app/scss/*.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('./app/css/'))
+            .pipe(browserSync.stream());
+    })
+    //tạo một task browser sync -reload trình duyệt
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './app'
+        }
+    });
+    gulp.watch('app/scss/*.scss').on('change', gulp.series('sass'));
+    gulp.watch('app/**').on('change', browserSync.reload);
+})
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'browser-sync'], callback);
+});
